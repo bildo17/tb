a97f5da1e4f47a3acc13498e0976b77d32d4ce01
Bill Okoba <bildadmo@gmail.com>
2018-10-29 13:48:58 +0300

    content on idex

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
-
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send({#issa successfull deploy})
+});
 
 passport.use(new GoogleStrategy());
 

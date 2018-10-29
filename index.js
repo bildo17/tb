6b77d9148b484e18a9e34a196a64f1b63750ac8d
Bill Okoba <bildadmo@gmail.com>
2018-10-29 15:59:53 +0300

    added keys

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({issa successfull deploy})
-});
+//app.get('/', (req, res) => {
+//res.send({issa successfull deploy})
+//});
 
 passport.use(new GoogleStrategy());
-
+// clientID 47054565510-0iul64ng2le7nvjd0obqhgfv9svhbqhp.apps.googleusercontent.com
+//clientSecret gYm4S5PaNNzSk1IzpKdfvY_P
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

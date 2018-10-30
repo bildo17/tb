3206cc5bf82dbbbca3bff655bf7269fc824533c7
Bill Okoba <bildadmo@gmail.com>
2018-10-30 09:55:37 +0300

    new

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
+const GoogleStrategy = require("passport-google-oauth2");
+const keys = require("./config/keys.js");
+
 const app = express();
 
 //app.get('/', (req, res) => {
 //res.send({issa successfull deploy})
 //});
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callBackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 // clientID 47054565510-0iul64ng2le7nvjd0obqhgfv9svhbqhp.apps.googleusercontent.com
 //clientSecret gYm4S5PaNNzSk1IzpKdfvY_P
 const PORT = process.env.PORT || 5000;

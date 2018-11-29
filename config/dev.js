ae9896132153d1d1c8166ca70312eb740cbf14d4
Bill Okoba <bildadmo@gmail.com>
2018-11-29 21:00:26 +0300

    added dev keys

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "47054565510-0iul64ng2le7nvjd0obqhgfv9svhbqhp.apps.googleusercontent.com",
+  googleClientSecret: "gYm4S5PaNNzSk1IzpKdfvY_P",
+  databaseID: "mongodb://clients:townboy17@ds151513.mlab.com:51513/tbclient",
+  cookieKey: "csfc3ewrgvrebvrevrecew",
+  googleCallBackUrl: "http://localhost:5000/auth/google/callback"
+};
